36b26cd23ffd2bbd3330f3555fcf03156efa1501
Altsoft-KimDongYoung <kim.dy@altsoft.kr>
2025-04-09 17:32:21 +0900

    시리얼번호

--- app/index.tsx
+++ app/index.tsx
@@ -4,12 +4,14 @@ import { useState, useEffect } from "react";
 
 export default function Index() {
   const [uniqueId, setUniqueId] = useState("");
+  const [serialNumber, setSerialNumber] = useState("");
   const appId = DeviceInfo.getBundleId();
   const buildNumber = DeviceInfo.getBuildNumber();
   const version = DeviceInfo.getVersion();
 
   useEffect(() => {
     DeviceInfo.getUniqueId().then((id) => setUniqueId(id));
+    DeviceInfo.getSerialNumber().then((serial) => setSerialNumber(serial));
   }, []);
 
   return (
@@ -24,6 +26,7 @@ export default function Index() {
       <Text>빌드 번호: {buildNumber}</Text>
       <Text>버전: {version}</Text>
       <Text>고유 ID: {uniqueId}</Text>
+      <Text>시리얼 번호: {serialNumber}</Text>
     </View>
   );
 }
